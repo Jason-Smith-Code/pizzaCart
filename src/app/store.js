5e11e394482f134114857c7326e999d3e33c720d
jason-smith-code <jasonsmithcode@gmail.com>
2021-12-17 09:39:21 +0000

    imported 2 reducers, made a configureStore, and added those reducers

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice'; // we name our reducer her since the export is default
+import isLoggedReducer from '../features/login/loginSlice'; // we name our reducer her since the export is default
 
 // here i can import all my reducers from my slices, i think need to assign them n the configure store.
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    login: isLoggedReducer
   },
 });

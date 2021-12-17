c30f0d72d927d11bfffbbc0252f18b9a3de87846
jason-smith-code <jasonsmithcode@gmail.com>
2021-12-17 09:37:23 +0000

    Add main contents of App, imported them from other files

--- src/app/App.js
+++ src/app/App.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import './App.css';
+import { ProductCategories } from '../features/ProductCategories/productCategories';
 import { Counter } from '../features/counter/Counter';
 import { Login } from '../features/login/Login';
+import { Cart } from '../features/Cart/Cart';
+import { ProductList } from '../features/ProductList/productList';
 import logo from './logo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo}></img>
-        <Login />
-      </header>
-      <div className="main">
+      <div className="contentWidth">
+        <header className="App-header">
+          <img alt="logo"src={logo}></img>
+          <Login />
+        </header>
+        <div className="main">
+          <div className="productCategoryContainer">
+            <h2 className="title">Menu</h2>
+            <ProductCategories />
+          </div>
+          <div className="productListContainer">
+            <h2 className="title">Product List</h2>
+            <ProductList/>
+          </div>
+          <div className="cartContainer">
+            <h2 className="title">Cart</h2>
+            <Cart />
+          </div>
+        </div>
         <Counter />
       </div>
+      
     </div>
   );
 }

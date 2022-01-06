cb4608faca2eb88bf68acb38792b33767f2379ce
jason-smith-code <jasonsmithcode@gmail.com>
2022-01-06 10:32:05 +0000

    Revised category list, created a map of the array and rendered that with the filter component

---
+++ src/components/Filter/Filters.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './Filter.css';
+import Filter from './Filter';
+
+// write out the array of categories here
+const categories = [
+  'All',
+  'Margarita', 
+  'Funghi', 
+  'Pepperoni', 
+  'Polo', 
+  'Salami', 
+  'Ham', 
+  'Prawn', 
+  'Milanese', 
+  'Carribean', 
+  'Pazza', 
+  'Bolognese', 
+  'Special Polo',
+  'Vegetarian',
+  'Venisera',
+  'Valentina',
+  'Toscana',
+  'Quindici',
+  'Quattro',
+  'Neptune',
+  'Pescatore',
+  'Meat Feast'
+];
+
+// Return a map of categories, supply 2 props to the Filter
+
+const Filters = () => {
+    return (
+        <div>
+          {categories.map(category =>
+            <Filter 
+              category={category}
+              key={category}
+            />)}
+        </div>
+    );
+}
+
+export default Filters;

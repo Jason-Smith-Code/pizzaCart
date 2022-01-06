ed792277e50ac4405f141c0ab2a571e699d31214
jason-smith-code <jasonsmithcode@gmail.com>
2022-01-06 10:37:17 +0000

    This file removed product.category from rendering as it was not needed

---
+++ src/components/Product/Product.js
@@ -0,0 +1,40 @@
+import React, { useState} from 'react'; 
+import './Product.css';
+import QuantityButton from './QuantityButton';
+
+import { useDispatch } from 'react-redux';
+import { addItemToCart } from '../../features/Cart/cartSlice';
+
+// here I am clarifying how the Product will render itself, I can reference the product property using dot notation
+
+const Product = ({product}) => {
+    const [quantity, setQuantity] = useState(1); 
+    const dispatch = useDispatch();
+
+    return (
+        <div className="productContainer">
+            <div className="productImage"></div>
+            <div className="productInfo">
+                <h2>{product.title}</h2>
+                <p>{product.subtitle}</p>
+                <p className="size">Size: {product.size}</p>
+                {/* use toFixed on price to force 2 decimal places */}
+                <p className="Price">{product.price.toFixed(2)}</p>  
+            </div>
+            <div className="addToCart">
+                <QuantityButton 
+                    quantity={quantity}
+                    setQuantity={setQuantity}
+                />
+                <button
+                    onClick={() => {
+                        dispatch(addItemToCart({product, quantity}))
+                    }}
+                
+                >Add to Cart</button>
+            </div>
+        </div>
+    )
+}
+
+export default Product;
